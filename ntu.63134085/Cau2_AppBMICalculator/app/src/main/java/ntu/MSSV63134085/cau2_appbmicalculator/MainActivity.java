de93977f6173a88f0818d7fc5ae7957003954725
cuhoangdk <104707960+cuhoangdk@users.noreply.github.com>
2024-03-23 18:06:10 +0700

    Câu 2 BE WIP (push lần 3)
    Câu 2 BE WIP (push lần 3)

--- ntu.63134085/Cau2_AppBMICalculator/app/src/main/java/ntu/MSSV63134085/cau2_appbmicalculator/MainActivity.java
+++ ntu.63134085/Cau2_AppBMICalculator/app/src/main/java/ntu/MSSV63134085/cau2_appbmicalculator/MainActivity.java
@@ -3,12 +3,43 @@ package ntu.MSSV63134085.cau2_appbmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    RadioButton r_chauau, r_chaua;
+    EditText edtChieuCao;
+    EditText edtCanNang;
+    EditText edtKQ;
+    TextView tvCmt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getControls();
+    }
+    public void getControls(){
+        r_chaua = (RadioButton) findViewById(R.id.radioChauA);
+        r_chauau = (RadioButton) findViewById(R.id.radioChauAu);
+
+        tvCmt = (TextView) findViewById(R.id.tvComment);
+        edtChieuCao = (EditText) findViewById(R.id.edtHeight);
+        edtCanNang = (EditText) findViewById(R.id.edtWeight);
+        edtKQ = (EditText) findViewById(R.id.edtKQ);
+    }
+    public void BMICalculator(){
+        try {
+            double chieuCao = Double.parseDouble(edtChieuCao.getText().toString());
+            double canNang = Double.parseDouble(edtCanNang.getText().toString());
+            double BMI = canNang/(chieuCao*chieuCao);
+            edtKQ.setText(String.valueOf(BMI));
+
+        }
+        catch (NumberFormatException e)
+        {
+            Toast.makeText(this, "Vui lòng nhập dữ liệu", Toast.LENGTH_SHORT).show();
+        }
     }
 }
